7f01948a0a024dd00c248c52ba0cc89e58f40d9e
[thepus5h] <[pushpaja564@gmail.com]>
2024-08-09 19:21:33 +0530

    update

---
+++ addprod.py
@@ -0,0 +1,18 @@
+n=int(input('enter n number: '))
+s=0
+p=1
+for i in range(1,n+1,1):
+    num=int(input("Enter the value: "))
+    s=num+s
+    p=num*p
+print("sum=",s)
+print("product=",p)
+
+
+    
+    
+    
+
+
+
+                
---
+++ for.py
@@ -0,0 +1,6 @@
+x=int(input("enter x value: "))
+for i in range(1,x+1,1):        
+    if(i%5==0):
+        print('hello')
+    else:
+        print('bye')    
